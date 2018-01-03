3fd730f54ad0915baf4d9aa1b4b9a18e6b8b092f
mwaz <amby.mwaz@gmail.com>
2018-01-03 15:58:55 +0300

    [ft] url to recipe creation form

--- yummy/yummyPI/urls.py
+++ yummy/yummyPI/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . views import home, create_category, view_created_categories
+from . views import home, create_category, view_created_categories, create_recipe
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', view_created_categories, name='home'),
     url(r'^Register/$', views.register),
     url(r'^Category/$', create_category,   name='category'),
+    url(r'^Recipe/$', create_recipe,   name='recipe'),
     url(r'^Login/$', auth_views.login,  {'template_name': 'login.html'}, name='login'),
     url(r'^Logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout')
 ]
