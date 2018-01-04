34235acf46ff9b7245d3b67592d235351b19c5d3
mwaz <amby.mwaz@gmail.com>
2018-01-05 00:07:05 +0300

    [ft] add created_by foreign key to categories model

--- yummy/yummyPI/models.py
+++ yummy/yummyPI/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.forms import ModelForm
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Categories(models.Model):
     category_name = models.CharField(max_length=256)
     category_description = models.CharField(max_length=256)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.category_name
